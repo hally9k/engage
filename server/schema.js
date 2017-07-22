e358a9b0df7c1ad52017487c059e1ab1d033d322
hally9k <hally9k@gmail.com>
2017-07-22 21:59:11 +1200

    Added graphql types

--- server/schema.js
+++ server/schema.js
@@ -1,17 +1,10 @@
-const gql = require('graphql')
-const User = require('./modules/user')
-
-const { GraphQLSchema, GraphQLList, GraphQLObjectType } = gql
-
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '',
-        database: 'engage'
-    }
-})
+const User = require('./type/user')
+const Child = require('./type/child')
+const Subject = require('./type/subject')
+const Activity = require('./type/activity')
+const Session = require('./type/session')
+const { GraphQLSchema, GraphQLList, GraphQLObjectType } = require('graphql')
+const sql = require('./connector/sql')
 
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -21,46 +14,71 @@ const schema = new GraphQLSchema({
                 type: new GraphQLList(User),
                 resolve: (_, args) => {
                     return args.id
-                        ? knex
+                        ? sql
                             .select()
                             .from('user')
                             .where('id', args.id)
                             .then((user) => user)
-                        : knex.select().from('user').then((users) => users)
+                        : sql.select().from('user').then((users) => users)
                 }
-            } // ,
-            // child: {
-            //   type: new GraphQLList(Child),
-            //   resolve: (_, args) => {
-            //     return args.id
-            //     ? knex.select().from('child').where('id', args.id).then(child => child)
-            //     : knex.select().from('child').then(children => children)
-            //   }
-            // },
-            // subject: {
-            //   type: new GraphQLList(Subject),
-            //   resolve: (_, args) => {
-            //     return args.id
-            //     ? knex.select().from('subject').where('id', args.id).then(subject => subject)
-            //     : knex.select().from('subject').then(subjects => subjects)
-            //   }
-            // },
-            // activity: {
-            //   type: new GraphQLList(Activity),
-            //   resolve: (_, args) => {
-            //     return args.id
-            //     ? knex.select().from('activity').where('id', args.id).then(activity => activity)
-            //     : knex.select().from('activity').then(activitys => activitys)
-            //   }
-            // },
-            // session: {
-            //   type: new GraphQLList(Activity),
-            //   resolve: (_, args) => {
-            //     return args.id
-            //     ? knex.select().from('session').where('id', args.id).then(session => session)
-            //     : knex.select().from('session').then(sessions => sessions)
-            //   }
-            // }
+            },
+            child: {
+                type: new GraphQLList(Child),
+                resolve: (_, args) => {
+                    return args.id
+                        ? sql
+                            .select()
+                            .from('child')
+                            .where('id', args.id)
+                            .then((child) => child)
+                        : sql.select().from('child').then((children) => children)
+                }
+            },
+            subject: {
+                type: new GraphQLList(Subject),
+                resolve: (_, args) => {
+                    return args.id
+                        ? sql
+                            .select()
+                            .from('subject')
+                            .where('id', args.id)
+                            .then((subject) => subject)
+                        : sql
+                            .select()
+                            .from('subject')
+                            .then((subjects) => subjects)
+                }
+            },
+            activity: {
+                type: new GraphQLList(Activity),
+                resolve: (_, args) => {
+                    return args.id
+                        ? sql
+                            .select()
+                            .from('activity')
+                            .where('id', args.id)
+                            .then((activity) => activity)
+                        : sql
+                            .select()
+                            .from('activity')
+                            .then((activitys) => activitys)
+                }
+            },
+            session: {
+                type: new GraphQLList(Session),
+                resolve: (_, args) => {
+                    return args.id
+                        ? sql
+                            .select()
+                            .from('session')
+                            .where('id', args.id)
+                            .then((session) => session)
+                        : sql
+                            .select()
+                            .from('session')
+                            .then((sessions) => sessions)
+                }
+            }
         }
     })
 })
