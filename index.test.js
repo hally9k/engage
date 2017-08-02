10c18f787f156eab903985d77bb6b5513c077256
hally9k <hally9k@gmail.com>
2017-08-02 18:49:22 +1200

    Fixed tests and linting for mocha nd chai.

--- index.test.js
+++ index.test.js
@@ -1,3 +1,5 @@
-describe('Placeholder test.', () => {
-    expect(true).to.equal(true)
+describe('Placeholder test', () => {
+    it('tests that true is in fact true.', () => {
+        expect(true).to.equal(true)
+    })
 })
