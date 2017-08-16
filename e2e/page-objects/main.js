4e0700ad9178fca4cfdba9964b3b58e3a31bcece
hally9k <hally9k@gmail.com>
2017-08-16 17:20:45 +1200

    ...

---
+++ e2e/page-objects/main.js
@@ -0,0 +1,23 @@
+const ONE_THOUSAND = 1000
+
+let mainCommands = {
+    mainLoad: function() {
+        return this.waitForElementVisible('@activities', ONE_THOUSAND)
+    },
+}
+
+module.exports = {
+    commands: [mainCommands],
+    url: function() {
+        return this.api.launchUrl
+    },
+    elements: {
+        activities: {
+            selector: '.activities',
+            locateStrategy: 'css',
+        },
+        error: {
+            selector: '.error',
+        },
+    },
+}
