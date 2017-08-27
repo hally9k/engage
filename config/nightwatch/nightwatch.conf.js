4b6d77f48ce55491f7f73190ced22f940f199974
hally9k <hally9k@gmail.com>
2017-08-27 14:22:56 +1200

    Tweaking for travis saucelabs

--- config/nightwatch/nightwatch.conf.js
+++ config/nightwatch/nightwatch.conf.js
@@ -22,7 +22,7 @@ module.exports = {
 
     test_settings: {
         default: {
-            launch_url: 'http://ondemand.saucelabs.com:8080',
+            launch_url: 'http://ondemand.saucelabs.com:80',
             selenium_port: 80,
             selenium_host: 'ondemand.saucelabs.com',
             silent: true,
