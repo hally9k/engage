6af3d93cfc385df6732005a60904ee4a15bae9d5
hally9k <hally9k@gmail.com>
2017-08-27 15:05:33 +1200

    trying tweaks.

--- config/nightwatch/nightwatch.conf.js
+++ config/nightwatch/nightwatch.conf.js
@@ -9,16 +9,16 @@ module.exports = {
     test_workers: false, // true will to run tests in parallel (http://nightwatchjs.org/guide#parallel-running)
     globals_path: '',
 
-    selenium: {
-        start_process: true,
-        server_path: seleniumServer.path,
-        host: '127.0.0.1',
-        port: 4444,
-        cli_args: {
-            'webdriver.chrome.driver': 'config/nightwatch/drivers/chromedriver',
-            'webdriver.gecko.driver': 'config/nightwatch/drivers/geckodriver',
-        },
-    },
+    // selenium: {
+    //     start_process: true,
+    //     server_path: seleniumServer.path,
+    //     host: '127.0.0.1',
+    //     port: 4444,
+    //     cli_args: {
+    //         'webdriver.chrome.driver': 'config/nightwatch/drivers/chromedriver',
+    //         'webdriver.gecko.driver': 'config/nightwatch/drivers/geckodriver',
+    //     },
+    // },
 
     test_settings: {
         default: {
@@ -50,23 +50,23 @@ module.exports = {
                 },
             },
         },
-        local: {
-            launch_url: 'http://localhost:8000',
-            selenium_host: '127.0.0.1',
-            selenium_port: 4444,
-            pathname: '/wd/hub',
-            silent: true,
-            screenshots: {
-                enabled: false,
-                path: '',
-            },
-
-            desiredCapabilities: {
-                browserName: 'chrome',
-                chromeOptions: {
-                    // "args" : ["headless", "no-sandbox", "disable-gpu"] //Enabling  this will start chrome in headless mode
-                },
-            },
-        },
+        // local: {
+        //     launch_url: 'http://localhost:8000',
+        //     selenium_host: '127.0.0.1',
+        //     selenium_port: 4444,
+        //     pathname: '/wd/hub',
+        //     silent: true,
+        //     screenshots: {
+        //         enabled: false,
+        //         path: '',
+        //     },
+        //
+        //     desiredCapabilities: {
+        //         browserName: 'chrome',
+        //         chromeOptions: {
+        //             // "args" : ["headless", "no-sandbox", "disable-gpu"] //Enabling  this will start chrome in headless mode
+        //         },
+        //     },
+        // },
     },
 }
