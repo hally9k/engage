87266b60084354a2ef48ede2be6725f24ddfa124
hally9k <hally9k@gmail.com>
2017-09-19 11:32:46 +1200

    ...

---
+++ app/duck/message.js
@@ -0,0 +1,80 @@
+import graphql from 'utility/graphql'
+import { normalize } from 'normalizr'
+import { processed, PROCESSED } from 'duck'
+import { conversationSchema } from 'schema'
+import messageMutation from 'graphql/mutation/message'
+import { fromJS, Map } from 'immutable'
+
+// Actions
+const SENDING_NEW_MESSAGE = 'conversation/SENDING_NEW_MESSAGE'
+const RECEIVED_NEW_MESSAGE = 'conversation/RECEIVED_NEW_MESSAGE'
+const RECEIVED_NEW_MESSAGE_WITH_ERRORS =
+    'conversation/RECEIVED_NEW_MESSAGE_WITH_ERRORS'
+
+export const sendingNewMessage = (
+    content,
+    userId,
+    conversationId,
+    channel,
+) => ({
+    type: SENDING_NEW_MESSAGE,
+    payload: {
+        content,
+        userId,
+        conversationId,
+        channel,
+    },
+})
+
+export const receivedNewMessage = payload => ({
+    type: RECEIVED_NEW_MESSAGE,
+    payload,
+})
+
+export const receivedNewMessageWithErrors = payload => ({
+    type: RECEIVED_NEW_MESSAGE_WITH_ERRORS,
+    payload,
+})
+
+// Reducers
+const INITIAL_STATE = Map()
+
+export default (state = INITIAL_STATE, action) => {
+    if (!action) return state
+    switch (action.type) {
+        case PROCESSED:
+            return state.merge(fromJS(action.payload.entities.message))
+        default:
+            return state
+    }
+}
+
+// Epics
+
+export const sendingNewMessageEpic = action$ =>
+    action$
+        .ofType(SENDING_NEW_MESSAGE)
+        .mergeMap(({ payload: { content, userId, conversationId, channel } }) =>
+            graphql
+                .request(messageMutation, {
+                    content,
+                    userId,
+                    conversationId,
+                    channel,
+                })
+                .then(message => {
+                    return [receivedNewMessage(message)]
+                }),
+        )
+
+export const receivedNewMessageEpic = action$ =>
+    action$
+        .ofType(RECEIVED_NEW_MESSAGE)
+        .mergeMap(({ payload: conversations }) => {
+            return [processed(normalize(conversations, conversationSchema))]
+        })
+
+export const epics = {
+    sendingNewMessageEpic,
+    receivedNewMessageEpic,
+}
