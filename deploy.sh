5d9e74c26a490e2bafd18609c069533ec07ea5b3
hally9k <hally9k@gmail.com>
2017-10-22 23:13:05 +1300

    ...

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker-machine scp -r /Users/hally9k/Repos/Engage/docker-stack.yml engage-machine:/home/engage/docker-stack.yml
-docker-machine scp -r /Users/hally9k/Repos/Engage/server/database/ engage-machine:/home/engage/server/
-dm ssh engage-machine
-docker stack deploy --compose-file docker-stack.yml engage-stack
