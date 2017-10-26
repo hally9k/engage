2fcca238b916882506ad5b1d20b5c6d6636b35b7
hally9k <hally9k@gmail.com>
2017-10-26 22:42:22 +1300

    Added deploy-bot service script.

--- devops/provision.sh
+++ devops/provision.sh
@@ -2,9 +2,11 @@ docker-machine create --driver digitalocean --digitalocean-image ubuntu-16-04-x6
 docker-machine scp -r ./docker-stack.yml engage-machine:/home/engage/docker-stack.yml
 docker-machine scp -r ./server/database/ engage-machine:/home/engage/server/
 docker-machine scp -r ./devops/deploy-bot engage-machine:/home/engage/deploy-bot
+docker-machine scp -r ./devops/deploy-bot-service engage-machine:/etc/init.d/deploy-bot
 
-docker-machine ssh engage-machine
+docker-machine env engage-machine
 cd /home/engage
 chmod +x deploy-bot
 chmod 755 deploy.sh
-./deploy-bot &
+update-rc.d deplot-bot defaults
+service deploy-bot start
