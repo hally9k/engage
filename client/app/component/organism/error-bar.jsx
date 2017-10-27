64dfc0ac38db61ce8c0dd790fd58623c00c47c49
hally9k <hally9k@gmail.com>
2017-10-27 14:41:39 +1300

    Switched up to test ci

--- client/app/component/organism/error-bar.jsx
+++ client/app/component/organism/error-bar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Error = ({ error }) => <h1>Error: {error}</h1>
+const Error = ({ error }) => <h1>Errorrrrr: {error}</h1>
 
 Error.propTypes = {
     error: PropTypes.object
